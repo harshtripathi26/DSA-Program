b348fdd3ad1a283474d877608d13b325617ef64a
harsh tripathi <tripathiharsh320@gmail.com>
2025-11-04 23:01:35 +0530

    Update Area of Triangle program

--- src/Assignment/AofTriangle.java
+++ src/Assignment/AofTriangle.java
@@ -1,4 +1,18 @@
 package Assignment;
 
+import java.util.Scanner;
+
 public class AofTriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Using Base and Height
+        System.out.println("Enter Base(cm): ");
+        int base= in.nextInt();
+        System.out.println("Enter Height(cm): ");
+        int height = in.nextInt();
+
+        int AreaofTriangle = (base * height)*1/2;
+        System.out.println("Area of Triangle: " + AreaofTriangle);
+    }
 }
